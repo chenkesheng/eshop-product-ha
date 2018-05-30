4a3d57ec9ac36e4e1bb12a212ace8903063b13e0
cks <kesheng.chen@molbase.com>
2018-05-30 14:30:13 +0800

    修改返回值

--- src/main/java/com/roncoo/eshop/product/ha/controller/ProductController.java
+++ src/main/java/com/roncoo/eshop/product/ha/controller/ProductController.java
@@ -1,5 +1,7 @@
 package com.roncoo.eshop.product.ha.controller;
 
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,5 +18,16 @@ public class ProductController {
     @GetMapping("/getProductInfo")
     public String getProductInfo(Long productId) {
         return "{\"id\": " + productId + ", \"name\": \"iphone7手机\", \"price\": 5599, \"pictureList\":\"a.jpg,b.jpg\", \"specification\": \"iphone7的规格\", \"service\": \"iphone7的售后服务\", \"color\": \"红色,白色,黑色\", \"size\": \"5.5\", \"shopId\": 1, \"modifiedTime\": \"2018-01-01 12:00:00\", \"cityId\": 1, \"brandId\": 1}";
+    }
+
+    @GetMapping("/getProductInfos")
+    public String getProductInfos(String productIds) {
+        System.out.println("getProductInfos接口，接收到一次请求，productIds=" + productIds);
+        JSONArray jsonArray = new JSONArray();
+        for(String productId : productIds.split(",")) {
+            String json = "{\"id\": " + productId + ", \"name\": \"iphone7手机\", \"price\": 5599, \"pictureList\":\"a.jpg,b.jpg\", \"specification\": \"iphone7的规格\", \"service\": \"iphone7的售后服务\", \"color\": \"红色,白色,黑色\", \"size\": \"5.5\", \"shopId\": 1, \"modifiedTime\": \"2017-01-01 12:00:00\", \"cityId\": 1, \"brandId\": 1}";
+            jsonArray.add(JSONObject.parseObject(json));
+        }
+        return jsonArray.toJSONString();
     }
 }
